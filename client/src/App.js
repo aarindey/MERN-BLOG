b44a8fb0da0b4c8cac31322e9b79b211af0e48d4
aarindey <aarindey069@gmail.com>
2024-02-23 19:51:55 +0530

    Client changes for auth + TailwindCSS configuration

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Blog from "./pages/Blog";
 import Header from "./components/Header";
 import BlogPage from "./pages/BlogPage";
 import BlogCommentPage from "./pages/BlogCommentPage";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/LogIn";
 
 const App = () => {
   return (
@@ -17,8 +19,10 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
           <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blog/new/:blogId" element={<BlogCommentPage/>} />
-          <Route path="/blogs/:blogId" element={<BlogPage/>} /> 
+          <Route path="/blog/new/:blogId" element={<BlogCommentPage />} />
+          <Route path="/blogs/:blogId" element={<BlogPage />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
     </Router>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top shadow-sm bg-white">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
-          <strong>Blog Overflow</strong>
+          <strong>IntelliBlog Hub</strong>
         </a>
         <button
           className="navbar-toggler"
@@ -18,15 +18,37 @@ const Header = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <div id="navbarSupportedContent">
+          <ul className="flex flex-row items-center space-x-4 ml-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a
+                className="text-gray-800 hover:text-gray-600"
+                href="/login"
+                aria-current="page"
+              >
+                LogIn
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="text-gray-800 hover:text-gray-600"
+                href="/signup"
+                aria-current="page"
+              >
+                SignUp
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="text-gray-800 hover:text-gray-600"
+                href="/"
+                aria-current="page"
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" href="/create">
+              <a className="text-gray-800 hover:text-gray-600" href="/create">
                 Create
               </a>
             </li>
